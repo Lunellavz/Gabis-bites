792d351b01692137f1ef647e4b53950627cc9efe
Gabriella <gabi.andino2000@gmail.com>
2022-04-20 21:19:24 -0700

    fixed javascript

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+function yourName(){
 let aname = prompt("What is your name?");
 document.write("Nice to meet you," + aname);
-
-let foodq = prompt("You love food?");
-
-if (foodq=== "Yes") {
-    
-    document.write("You've come to the right place!");
-    
 }
-else {
-      
-    document.write("I hope this helps!");
+
+function bites(){
+    let foodq = prompt("You love food?");
+    
+    if (foodq=== "Yes") {
+        
+        document.write("You've come to the right place!");
+        
+    }
+    else {
+          
+        document.write("I hope this helps!");
+    } 
 }
+
+yourName()
+
+document.write("<br />")
+
+bites()
+
+
